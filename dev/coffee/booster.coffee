6559d524282e967854f528b238d8821569026ada
hidex7777 <hidex7777@gmail.com>
2015-07-20 00:51:11 +0900

    cyclic timer

--- dev/coffee/booster.coffee
+++ dev/coffee/booster.coffee
@@ -1,36 +1,105 @@
 #reading booster
 'use strict'
 
+class Clock
+  constructor: (@id) ->
+    @timerId = null
+    @canvas = document.getElementById @id
+    if !@canvas || !@canvas.getContext
+      return false
+    @ctx = @canvas.getContext '2d'
+    @width = @canvas.width
+    @height = @canvas.height
+    #150, 150に移動（このコンテキストが固定）
+    @ctx.translate @width / 2, @height / 2
+    #コンテキストを1pxに伸縮
+    @ctx.scale @width / 300, @height / 300
+    #-90度回転（上向き）x方向だけ描画に使う。
+    @ctx.rotate -Math.PI / 2
+    @ctx.strokeStyle = "black"
+    @ctx.fillStyle = "white"
+    @ctx.lineCap = "butt"
+  #時計を描く
+  drawClock: ->
+    #分目盛
+    @ctx.save()
+    @ctx.lineWidth = 2
+    for i in [0...60]
+      @ctx.beginPath()
+      #6度回転 * 60回
+      @ctx.rotate Math.PI / 30
+      # x = 130からx = 150に描画（内から外へ）
+      @ctx.moveTo 130, 0
+      @ctx.lineTo 150, 0
+      @ctx.stroke()
+    @ctx.restore()
+    #時間目盛
+    @ctx.save()
+    @ctx.lineWidth = 8
+    for i in [0...12]
+      @ctx.beginPath()
+      #30度回転
+      @ctx.rotate Math.PI / 6
+      # x = 120からx = 150に描画
+      @ctx.moveTo 120, 0
+      @ctx.lineTo 150, 0
+      @ctx.stroke()
+    @ctx.restore()
+    #秒針初期値
+    #@s(0)
+    return
+  s: (@cursec = 0) ->
+    @ctx.save()
+    # 秒針は6度ずつ回転。初期値は0
+    @ctx.rotate Math.PI / 30 * @cursec
+    @ctx.strokeStyle = "#cc0000"
+    @ctx.lineWidth = 2
+    @ctx.beginPath()
+    # x = -10からx = 130に描画
+    @ctx.moveTo -10, 0
+    @ctx.lineTo 130, 0
+    @ctx.stroke()
+    @ctx.restore()
+    return
+  #秒針を回す
+  tickTack: ->
+    if !@canvas
+      return
+    @s @cursec
+    @cursec = if @cursec < 60 then @cursec + 1 else 0
+    return
+  tickStart: ->
+    if !@timerId
+      _clock = this
+      _clock.tickTack()
+      @timerId = setInterval ->
+        _clock.clearAll()
+        _clock.drawClock()
+        _clock.tickTack()
+        return
+      , 1000
+    return
+  tickStop: ->
+    clearInterval @timerId
+    @s 0
+    return
+  clearAll: ->
+    @ctx.clearRect -150, -150, 300, 300
+
 #読み込み終了時に実行
 window.onload = ->
-  drawCircle()
-  drawHand()
-  return
-
-#円を描く
-drawCircle = ->
-  canvas = document.getElementById('mycanvas')
-  if (!canvas || !canvas.getContext)
-    return false
-  ctx = canvas.getContext('2d')
-  ctx.beginPath();
-  ctx.arc(150, 150, 145, 0/180*Math.PI, 360/180*Math.PI)
-  ctx.fillStyle = "#FFFFFF"
-  ctx.fill()
-  return
-
-#秒針を描く
-drawHand = ->
-  canvas = document.getElementById('mycanvas')
-  if (!canvas || !canvas.getContext)
-    return false
-  ctx = canvas.getContext('2d')
-  #ctx.beginPath()
-  #ctx.moveTo(150, 150)
-  #ctx.lineTo(150, 10)
-  #ctx.rotate(30 * Math.PI / 180)
-  #ctx.translate(150, 150)
-  #ctx.stroke()
-  ctx.fillStyle = "#000000"
-  ctx.fillRect(150, 0, 2, 150)
+  clock = new Clock "mycanvas"
+  clock.drawClock()
+  clock.s()
+  $('#start').click ->
+    if $('#start').text() is "START"
+      clock.tickStart()
+      $('#start').text "STOP"
+    else
+      clock.tickStop()
+      clock.clearAll()
+      clock.drawClock()
+      clock.s()
+      $('#start').text "START"
+    return
   return
--- dist/js/booster.js
+++ dist/js/booster.js
@@ -1,34 +1,114 @@
 (function() {
   'use strict';
-  var drawCircle, drawHand;
+  var Clock;
+
+  Clock = (function() {
+    function Clock(id) {
+      this.id = id;
+      this.timerId = null;
+      this.canvas = document.getElementById(this.id);
+      if (!this.canvas || !this.canvas.getContext) {
+        return false;
+      }
+      this.ctx = this.canvas.getContext('2d');
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.ctx.translate(this.width / 2, this.height / 2);
+      this.ctx.scale(this.width / 300, this.height / 300);
+      this.ctx.rotate(-Math.PI / 2);
+      this.ctx.strokeStyle = "black";
+      this.ctx.fillStyle = "white";
+      this.ctx.lineCap = "butt";
+    }
+
+    Clock.prototype.drawClock = function() {
+      var i, _i, _j;
+      this.ctx.save();
+      this.ctx.lineWidth = 2;
+      for (i = _i = 0; _i < 60; i = ++_i) {
+        this.ctx.beginPath();
+        this.ctx.rotate(Math.PI / 30);
+        this.ctx.moveTo(130, 0);
+        this.ctx.lineTo(150, 0);
+        this.ctx.stroke();
+      }
+      this.ctx.restore();
+      this.ctx.save();
+      this.ctx.lineWidth = 8;
+      for (i = _j = 0; _j < 12; i = ++_j) {
+        this.ctx.beginPath();
+        this.ctx.rotate(Math.PI / 6);
+        this.ctx.moveTo(120, 0);
+        this.ctx.lineTo(150, 0);
+        this.ctx.stroke();
+      }
+      this.ctx.restore();
+    };
+
+    Clock.prototype.s = function(cursec) {
+      this.cursec = cursec != null ? cursec : 0;
+      this.ctx.save();
+      this.ctx.rotate(Math.PI / 30 * this.cursec);
+      this.ctx.strokeStyle = "#cc0000";
+      this.ctx.lineWidth = 2;
+      this.ctx.beginPath();
+      this.ctx.moveTo(-10, 0);
+      this.ctx.lineTo(130, 0);
+      this.ctx.stroke();
+      this.ctx.restore();
+    };
+
+    Clock.prototype.tickTack = function() {
+      if (!this.canvas) {
+        return;
+      }
+      this.s(this.cursec);
+      this.cursec = this.cursec < 60 ? this.cursec + 1 : 0;
+    };
+
+    Clock.prototype.tickStart = function() {
+      var _clock;
+      if (!this.timerId) {
+        _clock = this;
+        _clock.tickTack();
+        this.timerId = setInterval(function() {
+          _clock.clearAll();
+          _clock.drawClock();
+          _clock.tickTack();
+        }, 1000);
+      }
+    };
+
+    Clock.prototype.tickStop = function() {
+      clearInterval(this.timerId);
+      this.s(0);
+    };
+
+    Clock.prototype.clearAll = function() {
+      return this.ctx.clearRect(-150, -150, 300, 300);
+    };
+
+    return Clock;
+
+  })();
 
   window.onload = function() {
-    drawCircle();
-    drawHand();
-  };
-
-  drawCircle = function() {
-    var canvas, ctx;
-    canvas = document.getElementById('mycanvas');
-    if (!canvas || !canvas.getContext) {
-      return false;
-    }
-    ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.arc(150, 150, 145, 0 / 180 * Math.PI, 360 / 180 * Math.PI);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fill();
-  };
-
-  drawHand = function() {
-    var canvas, ctx;
-    canvas = document.getElementById('mycanvas');
-    if (!canvas || !canvas.getContext) {
-      return false;
-    }
-    ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(150, 0, 2, 150);
+    var clock;
+    clock = new Clock("mycanvas");
+    clock.drawClock();
+    clock.s();
+    $('#start').click(function() {
+      if ($('#start').text() === "START") {
+        clock.tickStart();
+        $('#start').text("STOP");
+      } else {
+        clock.tickStop();
+        clock.clearAll();
+        clock.drawClock();
+        clock.s();
+        $('#start').text("START");
+      }
+    });
   };
 
 }).call(this);
